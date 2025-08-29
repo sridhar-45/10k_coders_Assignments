ba08a51986ffa7e6e4b589be15478686ff70a1a7
sridhar goudu <sridhargoudu7@gmail.com>
2025-08-29 23:29:28 +0530

    Update basic_tasks.py

--- Python_and_Django/Datatypes_01_08_2025/basic_tasks.py
+++ Python_and_Django/Datatypes_01_08_2025/basic_tasks.py
@@ -1,1 +1,31 @@
-Python
+#1. Create 5 variables using different naming convetions(snake_case, camelCase, PascalCase, etc,).
+
+name = "harish"
+Age = 25
+_id_number = 20003434242
+instituteName = "10k coders"
+AddressDetails = "KPHB coloney"
+
+
+
+# 2. Create one variable of each type: int, float, str, bool, print their type and id()
+
+n = 5
+print(type(n))
+print(id(n))
+
+
+val = 12.34454
+print(type(val))
+print(id(val))
+
+
+s = "hello world"
+print(type(s))
+print(id(s))
+
+
+bool = True
+print(type(bool))
+print(id(bool))
+
