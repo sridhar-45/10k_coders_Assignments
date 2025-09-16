ddc1d6cf54a93c932827c58cb92fef4ea170646c
sridhar goudu <sridhargoudu7@gmail.com>
2025-09-16 12:24:56 +0530

    Update square_pattern.py

--- Python and Django/12-09-2025/square_pattern.py
+++ Python and Django/12-09-2025/square_pattern.py
@@ -1,1 +1,10 @@
 #   1.WAP to print square pattern using nested loop.
+# n = int(input())
+n = 5
+for i in range(n):
+    for j in range(n):
+        if  i == 0 or i == n-1 or j ==0 or j == n-1:
+            print("*",end = " ")
+        else:
+            print(" ",end =" ")
+    print()       
