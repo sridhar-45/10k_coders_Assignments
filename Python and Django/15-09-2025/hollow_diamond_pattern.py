42a85720fd23750977b5dd2592bfbe76c550caac
sridhar goudu <sridhargoudu7@gmail.com>
2025-09-25 17:07:24 +0530

    Update hollow_diamond_pattern.py

--- Python and Django/15-09-2025/hollow_diamond_pattern.py
+++ Python and Django/15-09-2025/hollow_diamond_pattern.py
@@ -12,3 +12,26 @@ for i in range(n, 0, -1):
     else:
         print(s + "* " + "  "*(i-2) + "*")
     
+
+
+
+##### hallow butterfly...
+n = 5
+print("* " + " " * 2 * (n-1) + "*")
+for i in range(n, 0, -1):
+    s =" " * (n-i)
+    if i == 1:
+        print("*" + s + "*"+ s + "*")
+    else:
+        print("*" + s + "* " + "  "*(i-2) + "*" + s + "*")
+        
+for i in range(2, n+1):
+    s = " " * (n-i)
+    # if i == 1:
+    #     print(s + "* ")
+    # else:
+    print("*" + s + "* " + "  "*(i-2) + "*"+ s + "*")
+print("* " + " "* 2*(n-1) + "*")
+
+
+    
